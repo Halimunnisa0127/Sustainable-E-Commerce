09156f481a5e144c0d5c7a291eb43f055b2977eb
Munni <shaikmunni1314@gmail.com>
2026-03-14 16:11:06 +0530

    "fix cors"

--- backend/CorsOptions/corsOptions.js
+++ backend/CorsOptions/corsOptions.js
@@ -1,14 +1,28 @@
 //corsOptions.js
 
-require("dotenv").config();
+require("dotenv").config()
+
+const allowedOrigins = [
+  process.env.CLIENT_URL,
+  "https://sustainable-e-commerce-8qd3.vercel.app",
+  "https://sustainable-e-commerce.vercel.app"
+]
 
 const corsOptions = {
-  origin: process.env.CLIENT_URL,
-  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  origin: function (origin, callback) {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true)
+    } else {
+      callback(new Error("Not allowed by CORS"))
+    }
+  },
+  methods: ["GET", "POST", "PUT", "DELETE"],
   allowedHeaders: ["Content-Type", "Authorization"],
   optionsSuccessStatus: 200
-};
+}
 
-module.exports = corsOptions;
+module.exports = corsOptions
+
+
 
 
