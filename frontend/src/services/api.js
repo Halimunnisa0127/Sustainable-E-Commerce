bf34fa040b14eb5540704c9c4c0852dc5e692382
Munni <shaikmunni1314@gmail.com>
2026-03-14 15:39:07 +0530

    "update url"

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -2,7 +2,7 @@
 import axios from "axios"
 
 const API = axios.create({
-  baseURL: "http://localhost:9000/api",
+  baseURL: process.env.REACT_APP_API_URL,
   timeout: 30000,
   headers: {
     "Content-Type": "application/json"
@@ -20,26 +20,3 @@ API.interceptors.response.use(
 
 export default API
 
-
-// // services/api.js - UPDATED VERSION
-// import axios from "axios"
-
-// // Use environment variable with fallback to localhost
-// const API = axios.create({
-//   baseURL: process.env.REACT_APP_API_URL || "http://localhost:9000/api",
-//   timeout: 30000,
-//   headers: {
-//     "Content-Type": "application/json"
-//   }
-// })
-
-// // Add response interceptor for debugging
-// API.interceptors.response.use(
-//   response => response,
-//   error => {
-//     console.error("API Error:", error.response?.data || error.message)
-//     return Promise.reject(error)
-//   }
-// )
-
-// export default API
